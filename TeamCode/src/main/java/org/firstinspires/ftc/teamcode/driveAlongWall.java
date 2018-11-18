ce25c3a0ccd515f32e6fb151ec087a16762428b2
DerpyUnlimited <geyer.clyde@gmail.com>
2018-11-18 14:16:32 -0500

    quickfix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveAlongWall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveAlongWall.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.util.TypeConversion;
@@ -11,6 +12,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.internal.opmode.TelemetryImpl;
 
 import RedStorm.Robot.Robot;
+
+@Autonomous(name="Drive Along Wall", group="distance")
 
 public class driveAlongWall extends LinearOpMode {
     public Robot robot = new Robot();    // Create a new instance of the robot
