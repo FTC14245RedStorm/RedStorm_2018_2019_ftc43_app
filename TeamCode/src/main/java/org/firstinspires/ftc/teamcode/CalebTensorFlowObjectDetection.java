9f260fa229cafe06a5e54889270f92c84b3098b9
anna <khakhalina.anna@gmail.com>
2019-01-02 14:57:34 -0500

    updated MineralDetection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalebTensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CalebTensorFlowObjectDetection.java
@@ -41,7 +41,7 @@ import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 import java.util.List;
 
-
+//@Disabled
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
 public class CalebTensorFlowObjectDetection extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
